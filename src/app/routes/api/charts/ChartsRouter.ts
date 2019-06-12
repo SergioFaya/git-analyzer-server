ecb94db92c50ee5412a17fea1539f339f62eb5a5
SergioFaya <estisergio@gmail.com>
2019-06-12 16:46:19 +0200

    remove dist from gitignore

--- src/app/routes/api/charts/ChartsRouter.ts
+++ src/app/routes/api/charts/ChartsRouter.ts
@@ -5,7 +5,7 @@ import NetworkChartService from '../../../services/business/impl/NetworkChartSer
 const router = Router();
 
 // hacer get repo paginado
-router.get('/charts/gitTree', (req: Request, res: Response): void => {
+router.get('/chart/gitTree', (req: Request, res: Response): void => {
 	const token = req.header('x-github-token') as string;
 	const reponame = req.header('reponame') as string;
 	const username = req.header('username') as string;
