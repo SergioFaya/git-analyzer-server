77a2129b1ef2f7af109058adc774c82da18ca4c2
SergioFaya <estisergio@gmail.com>
2019-07-09 19:56:24 +0200

    tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
 
 var should = require('should');
+var AuthService = require('../dist/app/services/business/impl/AuthService');
+
 describe('my first test', function() { 
 	it('is running', function() {
 		should('test' == 'test').be.ok;
 	});
 });
+
+describe('AuthServiceTest', ()=> {
+	describe('testAuth', ()=> {
+		AuthService.default.auth();
+	});
+})
